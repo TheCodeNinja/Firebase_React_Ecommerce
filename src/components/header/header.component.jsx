c44aa0eedb0916885286365ad4de798360802af8
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-05-19 19:26:34 +0800

    lesson 11 - add svg logo to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = () => (
   <div className="navbar">
     <Link className="navbar-logo" to='/'>
-      <div>Logo</div>
+      <Logo className='logo' />
     </Link>
     <div className="nav">
       <Link className="nav-link" to='/shop'>SHOP</Link>
