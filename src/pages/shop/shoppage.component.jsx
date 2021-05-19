81619004fb303831700cfdac8b19fb4acc645662
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-05-19 17:24:19 +0800

    lesson 08 - create category list component with dummy data

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
+import CategoryList from '../../components/category-list/category-list.component';
+import SHOP_DATA from './shop.data.js';
+
 class ShopPage extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      dummyListData: SHOP_DATA
+    }
+  }
+
   render() { 
-    return <div>Shop Page</div>
+    const { dummyListData } = this.state;
+    return (
+      <div>
+        {
+          dummyListData.map(({ id, ...otherProps }) => (
+            <CategoryList key={id} { ...otherProps } />
+          ))
+        }
+      </div> 
+    );
   };
 }
 
