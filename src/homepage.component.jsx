fe962824c0d41a352dcd89cc1debf32fec6b9479
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-05-19 14:08:16 +0800

    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+
+      {/* menu-item */}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            HATS
+          </h1>
+          <span className="subtitle">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+      {/* menu-item */}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            JACKETS
+          </h1>
+          <span className="subtitle">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+      {/* menu-item */}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            SNEAKERS
+          </h1>
+          <span className="subtitle">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+      {/* menu-item */}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            WOMEN
+          </h1>
+          <span className="subtitle">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+      {/* menu-item */}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            MEN
+          </h1>
+          <span className="subtitle">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default HomePage; 
