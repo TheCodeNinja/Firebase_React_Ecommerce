7d9c12bc0a9065fd56efa6b4234bd22b091fae85
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-05-20 20:28:27 +0800

    lesson 22 - redux implementation for setting current user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import HomePage from './pages/home/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import ShopPage from './pages/shop/shoppage.component';
 import Header from './components/header/header.component';
 import LoginSignupPage from './pages/login-signup/login-signup.component';
 import { auth, storeUserToFirestore } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 import './App.css';
 
@@ -21,6 +23,8 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    // destructuring the props coming from redux
+    const { setCurrentUser } = this.props;
     // Adds an observer for changes to the user's sign-in state.
     // Prior to 4.0.0, this triggered the observer when users were signed in, 
     // signed out, or when the user's ID token changed in situations such as 
@@ -31,17 +35,13 @@ class App extends Component {
         const userDocRef = await storeUserToFirestore(fbAuthUser);
         userDocRef.onSnapshot(userDocSnapShot => {
           // set current user
-          this.setState({
-            currentUser: {
-              id: userDocSnapShot.id,
-              ...userDocSnapShot.data()
-            }
-          }, () => {
-            // console.log(this.state);
+          setCurrentUser({
+            id: userDocSnapShot.id,
+            ...userDocSnapShot.data()
           });
         });
       } else {
-        this.setState({ currentUser: fbAuthUser });
+        setCurrentUser(fbAuthUser);
       }
     });
   }
@@ -54,7 +54,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
@@ -65,4 +65,9 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  // (this component props): (arrow function for dispatchiing action to reducer)
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser }) => ( // currentUser comes from redux
   <div className="navbar">
     <Link className="navbar-logo" to='/'>
       <Logo className='logo' />
@@ -30,4 +31,10 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header; 
+const mapStateToProps = state => ({
+  // (this component props): (redux state)
+  currentUser: state.user.currentUser
+});
+
+// connect(): let this component get access to the redux store 
+export default connect(mapStateToProps)(Header); 
